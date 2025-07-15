807ab52943be5f20361909502c07369a34559572
ChangwooJ <wjdckddn99@naver.com>
2025-07-15 20:20:07 +0900

    feat: 할 일 목록 구현

--- src/components/TodoTemplate.jsx
+++ src/components/TodoTemplate.jsx
@@ -25,22 +25,36 @@ const TodoTemplate = () => {
   const [todoList, setTodoList] = useState([]);
 
   const handleAddTodoItem = (item) => {
-    setTodoList((prev) => [
+    setTodoList((prev) => {
+      const nextId = prev.length > 0 ? prev[prev.length - 1].id + 1 : 1;
+      return [
       ...prev,
       {
-        id: todoList.length + 1,
+        id: nextId,
         text: item,
         checked: false,
       }
-    ])
+    ]})
   }
+
+  const handleToggle = (id) => {
+    setTodoList((prevList) =>
+      prevList.map((todo) =>
+        todo.id === id ? { ...todo, checked: !todo.checked } : todo
+      )
+    );
+  };
+
+  const handleDeleteTodo = (id) => {
+    setTodoList((prev) => prev.filter((todo) => todo.id !== id));
+  };
 
   return (
     <TodoListTemplateContainer>
       <TodoListTemplateWrapper>
         <Header />
         <TodoInsert onAddTodoItem={handleAddTodoItem} />
-        <TodoList />
+        <TodoList todoList={todoList} onToggle={handleToggle} onDeleteTodo={handleDeleteTodo} />
       </TodoListTemplateWrapper>
     </TodoListTemplateContainer>
   );
